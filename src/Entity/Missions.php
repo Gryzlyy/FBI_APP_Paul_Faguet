ecec19a6225351f5d58b11b748a3ccae9c5e8a82
Fagus <paul.faguet@free.fr>
2021-03-19 11:03:51 +0100

    entities, fbi db and controllers initialized

---
+++ src/Entity/Missions.php
@@ -0,0 +1,277 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MissionsRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MissionsRepository::class)
+ */
+class Missions
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $country;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $status;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Agents::class, inversedBy="missions")
+     */
+    private $agents;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Skills::class, inversedBy="missions")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $skills;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Targets::class, inversedBy="missions")
+     */
+    private $targets;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $startDate;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $endDate;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Contacts::class, mappedBy="missions")
+     */
+    private $contacts;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Hideouts::class, inversedBy="missions")
+     */
+    private $hideouts;
+
+    public function __construct()
+    {
+        $this->agents = new ArrayCollection();
+        $this->targets = new ArrayCollection();
+        $this->contacts = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): self
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Agents[]
+     */
+    public function getAgents(): Collection
+    {
+        return $this->agents;
+    }
+
+    public function addAgent(Agents $agent): self
+    {
+        if (!$this->agents->contains($agent)) {
+            $this->agents[] = $agent;
+        }
+
+        return $this;
+    }
+
+    public function removeAgent(Agents $agent): self
+    {
+        $this->agents->removeElement($agent);
+
+        return $this;
+    }
+
+    public function getSkills(): ?Skills
+    {
+        return $this->skills;
+    }
+
+    public function setSkills(?Skills $skills): self
+    {
+        $this->skills = $skills;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Targets[]
+     */
+    public function getTargets(): Collection
+    {
+        return $this->targets;
+    }
+
+    public function addTarget(Targets $target): self
+    {
+        if (!$this->targets->contains($target)) {
+            $this->targets[] = $target;
+        }
+
+        return $this;
+    }
+
+    public function removeTarget(Targets $target): self
+    {
+        $this->targets->removeElement($target);
+
+        return $this;
+    }
+
+    public function getStartDate(): ?string
+    {
+        return $this->startDate;
+    }
+
+    public function setStartDate(string $startDate): self
+    {
+        $this->startDate = $startDate;
+
+        return $this;
+    }
+
+    public function getEndDate(): ?string
+    {
+        return $this->endDate;
+    }
+
+    public function setEndDate(string $endDate): self
+    {
+        $this->endDate = $endDate;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Contacts[]
+     */
+    public function getContacts(): Collection
+    {
+        return $this->contacts;
+    }
+
+    public function addContact(Contacts $contact): self
+    {
+        if (!$this->contacts->contains($contact)) {
+            $this->contacts[] = $contact;
+            $contact->addMission($this);
+        }
+
+        return $this;
+    }
+
+    public function removeContact(Contacts $contact): self
+    {
+        if ($this->contacts->removeElement($contact)) {
+            $contact->removeMission($this);
+        }
+
+        return $this;
+    }
+
+    public function getHideouts(): ?Hideouts
+    {
+        return $this->hideouts;
+    }
+
+    public function setHideouts(?Hideouts $hideouts): self
+    {
+        $this->hideouts = $hideouts;
+
+        return $this;
+    }
+}
---
+++ var/cache/dev/url_matching_routes.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * This file has been auto-generated
+ * by the Symfony Routing Component.
+ */
+
+return [
+    false, // $matchHost
+    [ // $staticRoutes
+        '/_profiler' => [[['_route' => '_profiler_home', '_controller' => 'web_profiler.controller.profiler::homeAction'], null, null, null, true, false, null]],
+        '/_profiler/search' => [[['_route' => '_profiler_search', '_controller' => 'web_profiler.controller.profiler::searchAction'], null, null, null, false, false, null]],
+        '/_profiler/search_bar' => [[['_route' => '_profiler_search_bar', '_controller' => 'web_profiler.controller.profiler::searchBarAction'], null, null, null, false, false, null]],
+        '/_profiler/phpinfo' => [[['_route' => '_profiler_phpinfo', '_controller' => 'web_profiler.controller.profiler::phpinfoAction'], null, null, null, false, false, null]],
+        '/_profiler/open' => [[['_route' => '_profiler_open_file', '_controller' => 'web_profiler.controller.profiler::openAction'], null, null, null, false, false, null]],
+        '/admins' => [[['_route' => 'admins', '_controller' => 'App\\Controller\\AdminsController::index'], null, null, null, false, false, null]],
+        '/agents' => [[['_route' => 'agents', '_controller' => 'App\\Controller\\AgentsController::index'], null, null, null, false, false, null]],
+        '/contacts' => [[['_route' => 'contacts', '_controller' => 'App\\Controller\\ContactsController::index'], null, null, null, false, false, null]],
+        '/hideouts' => [[['_route' => 'hideouts', '_controller' => 'App\\Controller\\HideoutsController::index'], null, null, null, false, false, null]],
+        '/missions' => [[['_route' => 'missions', '_controller' => 'App\\Controller\\MissionsController::index'], null, null, null, false, false, null]],
+        '/skills' => [[['_route' => 'skills', '_controller' => 'App\\Controller\\SkillsController::index'], null, null, null, false, false, null]],
+        '/targets' => [[['_route' => 'targets', '_controller' => 'App\\Controller\\TargetsController::index'], null, null, null, false, false, null]],
+    ],
+    [ // $regexpList
+        0 => '{^(?'
+                .'|/_(?'
+                    .'|error/(\\d+)(?:\\.([^/]++))?(*:38)'
+                    .'|wdt/([^/]++)(*:57)'
+                    .'|profiler/([^/]++)(?'
+                        .'|/(?'
+                            .'|search/results(*:102)'
+                            .'|router(*:116)'
+                            .'|exception(?'
+                                .'|(*:136)'
+                                .'|\\.css(*:149)'
+                            .')'
+                        .')'
+                        .'|(*:159)'
+                    .')'
+                .')'
+            .')/?$}sDu',
+    ],
+    [ // $dynamicRoutes
+        38 => [[['_route' => '_preview_error', '_controller' => 'error_controller::preview', '_format' => 'html'], ['code', '_format'], null, null, false, true, null]],
+        57 => [[['_route' => '_wdt', '_controller' => 'web_profiler.controller.profiler::toolbarAction'], ['token'], null, null, false, true, null]],
+        102 => [[['_route' => '_profiler_search_results', '_controller' => 'web_profiler.controller.profiler::searchResultsAction'], ['token'], null, null, false, false, null]],
+        116 => [[['_route' => '_profiler_router', '_controller' => 'web_profiler.controller.router::panelAction'], ['token'], null, null, false, false, null]],
+        136 => [[['_route' => '_profiler_exception', '_controller' => 'web_profiler.controller.exception_panel::body'], ['token'], null, null, false, false, null]],
+        149 => [[['_route' => '_profiler_exception_css', '_controller' => 'web_profiler.controller.exception_panel::stylesheet'], ['token'], null, null, false, false, null]],
+        159 => [
+            [['_route' => '_profiler', '_controller' => 'web_profiler.controller.profiler::panelAction'], ['token'], null, null, false, true, null],
+            [null, null, null, null, false, false, 0],
+        ],
+    ],
+    null, // $checkCondition
+];
